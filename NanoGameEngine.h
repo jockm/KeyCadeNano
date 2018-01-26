bffd9a1356ca85be7fc719b04a95faaafb22598b
Jock Murphy <jock@jockmurphy.com>
2018-01-26 11:52:35 -0800

    Updated To include factory method, and Game End Key code

--- NanoGameEngine.h
+++ NanoGameEngine.h
@@ -12,13 +12,26 @@
 #	include "screen.h"
 #	include  "SSD1306.h"
 
+#	define NGE_NOKEY 0xFF
+#	define NGE_EXIT  0xFE
+
 	class NanoGameEngine {
 		public:
 			virtual ~NanoGameEngine() {};
 
+			virtual void loadMemory(uint8_t *mem, const uint8_t *prog, uint16_t progSize) {
+				memcpy(mem, prog, progSize);
+			}
+
 			virtual void init(uint8_t *prog, uint16_t memSize, Screen *screen) = 0;
 			virtual bool runOne(uint8_t keyPressed) = 0;
 			virtual void intervalCallback(void) {};
+
+			static NanoGameEngine *getGameEngine(uint8_t type)
+			{
+				// TODO should we move this?
+				return NULL;
+			}
 	};
 
 #endif /* NANOGAMEENGINE_H_ */

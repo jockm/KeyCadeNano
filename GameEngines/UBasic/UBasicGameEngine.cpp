49ebdfab4ce451236ec32bf8c26fe9ef779b03dd
Jock Murphy <jock@jockmurphy.com>
2018-02-07 07:03:35 -0800

    Initial Checkin of uBasic game engine

---
+++ GameEngines/UBasic/UBasicGameEngine.cpp
@@ -0,0 +1,18 @@
+/*
+ * UBasicGameEngine.cpp
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: jock
+ */
+
+#include <GameEngines/UBasic/UBasicGameEngine.h>
+
+UBasicGameEngine::UBasicGameEngine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UBasicGameEngine::~UBasicGameEngine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ GameEngines/UBasic/UBasicGameEngine.h
@@ -0,0 +1,19 @@
+/*
+ * UBasicGameEngine.h
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: jock
+ */
+
+#ifndef GAMEENGINES_UBASIC_UBASICGAMEENGINE_H_
+#define GAMEENGINES_UBASIC_UBASICGAMEENGINE_H_
+
+#include <NanoGameEngine.h>
+
+class UBasicGameEngine: public NanoGameEngine {
+public:
+	UBasicGameEngine();
+	virtual ~UBasicGameEngine();
+};
+
+#endif /* GAMEENGINES_UBASIC_UBASICGAMEENGINE_H_ */

80275c468de30626eac363e9b1e99fa5d786f6d3
Jock Murphy <jock@jockmurphy.com>
2018-02-08 12:58:44 -0800

    Initial checkin of base64 encode/decode

---
+++ base64.h
@@ -0,0 +1,32 @@
+/*********************************************************************
+* Filename:   base64.h
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Defines the API for the corresponding Base64 implementation.
+*********************************************************************/
+
+#ifndef BASE64_H
+#define BASE64_H
+
+/*************************** HEADER FILES ***************************/
+#include <stddef.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/*********************** FUNCTION DECLARATIONS **********************/
+// Returns the size of the output. If called with out = NULL, will just return
+// the size of what the output would have been (without a terminating NULL).
+size_t base64_encode(const uint8_t in[], uint8_t out[], size_t len, int newline_flag);
+
+// Returns the size of the output. If called with out = NULL, will just return
+// the size of what the output would have been (without a terminating NULL).
+size_t base64_decode(const uint8_t in[], uint8_t out[], size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif   // BASE64_H

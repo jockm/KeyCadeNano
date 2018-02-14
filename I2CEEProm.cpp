2839886041407dc49ba0b56e1ef0c3b1170d0bb6
Jock Murphy <jock@jockmurphy.com>
2018-02-14 06:55:21 -0800

    Initial checkin

---
+++ I2CEEProm.cpp
@@ -0,0 +1,14 @@
+/*
+ * I2CEEProm.cpp
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: jock
+ */
+
+#include <I2CEEProm.h>
+
+
+I2CEEProm::~I2CEEProm() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ I2CEEProm.h
@@ -0,0 +1,30 @@
+/*
+ * I2CEEProm.h
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: jock
+ */
+
+#ifndef I2CEEPROM_H_
+#	define I2CEEPROM_H_
+
+#	include "mbed.h"
+
+	class I2CEEProm {
+		public:
+			I2CEEProm(I2C *i2cPort, uint8_t addr)
+			{
+				this->i2c = i2cPort;
+				this->i2cAddr = addr;
+			}
+
+			virtual ~I2CEEProm();
+
+			uint8_t readByte(uint16_t addr);
+			uint16_t read(uint16_t addr, uint8_t *buf, uint16_t size);
+		private:
+			I2C     *i2c;
+			uint8_t  i2cAddr;
+	};
+
+#endif /* I2CEEPROM_H_ */

86ae24865fd6e3189aa08c657e5babdd447fab6b
Jock Murphy <jock@jockmurphy.com>
2018-02-23 15:26:22 -0800

    Fixed include for itoa

--- GameEngines/UBasic/UBasicGameEngine.cpp
+++ GameEngines/UBasic/UBasicGameEngine.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <stdlib.h>
 #include <b64.h>
 
 #include <GameEngines/UBasic/UBasicGameEngine.h>
